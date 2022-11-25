063b180636ba08a26dc2b29d3ef9ca95eb25b152
Paz Spera <mspera03@gmail.com>
2022-11-25 02:00:28 -0300

    Creé MovieCastComponent. Recibe currentMovie pero la devuelve null en consola. Igual usando v-if recorre el array de cast y lo imprime en el dom

---
+++ viernes-vue/src/components/MovieDetails/MovieCastComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="main-text" v-if="currentMovie">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="main__titulo">Elenco</h2>
+        </div>
+      </div>
+      <div class="row">
+        <figure class="pelicula__actor col-6 col-sm-4">
+          <img src="" alt="" />
+          <figcaption class="pelicula__actor__name text-center"></figcaption>
+        </figure>
+      </div>
+    </div>
+
+    <ul v-if="currentMovie">
+      <li v-for="actor in currentMovie.cast" :key="actor">{{ actor }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MovieCastComponent",
+  props: {
+    currentMovie: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    console.log(this.currentMovie);
+  },
+};
+</script>
+
+<style></style>
--- viernes-vue/src/views/MovieDetailsView.vue
+++ viernes-vue/src/views/MovieDetailsView.vue
@@ -2,7 +2,8 @@
   <MovieHeroComponent :currentMovie="currentMovie"></MovieHeroComponent>
 
   <main class="main-content">
-    <div class="main-text">
+    <!-- Sinopsis -->
+    <section class="main-text">
       <div class="container">
         <div class="col-12 col-md-8 col-lg-6">
           <div class="pelicula__sinopsis">
@@ -11,21 +12,24 @@
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
+    <!-- Cast -->
+    <MovieCastComponent :currentMovie="currentMovie"></MovieCastComponent>
   </main>
 </template>
 
 <script>
 import allMoviesJSON from "@/assets/data/info_movies1.json";
 import MovieHeroComponent from "@/components/MovieDetails/MovieHeroComponent.vue";
+import MovieCastComponent from "@/components/MovieDetails/MovieCastComponent.vue";
 
 export default {
   name: "MovieDetailsView",
-  components: { MovieHeroComponent },
+  components: { MovieHeroComponent, MovieCastComponent },
   props: ["id"],
   mounted() {
     this.currentMovie = allMoviesJSON.find((movie) => movie.id === this.id);
-    console.log(this.currentMovie);
   },
   data() {
     return {
