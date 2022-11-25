b5f472cff000a068a6fb75b21f60aadd0628c7ba
Paz Spera <mspera03@gmail.com>
2022-11-25 19:51:57 -0300

    Logré que recupere y muestre las img y alt de elencogit add . \o/ También agregué estilo de layout películas a main.scss

--- viernes-vue/src/components/MovieDetails/MovieCastComponent.vue
+++ viernes-vue/src/components/MovieDetails/MovieCastComponent.vue
@@ -7,24 +7,12 @@
         </div>
       </div>
       <div class="row" v-if="currentMovie">
-        <figure v-for="actor in currentMovie.cast" :key="actor" class="pelicula__actor col-6 col-sm-4">
-          <img :src="currentMovie.cast.img" :alt="currentMovie.cast.alt" />
-          <figcaption class="pelicula__actor__name text-center">{{ currentMovie.cast.alt }}</figcaption>
+        <figure v-for="(actor, index) in currentMovie.cast" :key="index" class="pelicula__actor col-6 col-sm-4">
+          <img :src="currentMovie.cast[index].img" :alt="currentMovie.cast[index].actor" />
+          <figcaption class="pelicula__actor__name text-center">{{ currentMovie.cast[index].actor }}</figcaption>
         </figure>
       </div>
     </div>
-
-    <ul v-if="currentMovie">
-      <li v-for="image in currentMovie.cast_2" :key="image.alt">{{ image }}</li>
-    </ul>
-
-    <div v-if="currentMovie">
-      <p>{{ currentMovie.cast_2[0] }}</p>
-    </div>
-
-    <!-- <ul v-if="currentMovie">
-      <li v-for="actor in currentMovie.cast" :key="actor">{{ actor.img }} - {{ actor.actor }}</li>
-    </ul> -->
   </section>
 </template>
 
