6cb254159823ebba49eb0a31a99a3d608fc94835
Paz Spera <mspera03@gmail.com>
2022-11-25 02:26:21 -0300

    Estoy tratando de mostrar las img del Elenco. No está funcionando bien. Con el cast no puedo bindear al src y alt. Traté de hacer cast_2 para pasar un objeto de imagen pero no renderea nada. Al menos con cast puedo recuperar los nombres de archivo y actor

--- viernes-vue/src/components/MovieDetails/MovieCastComponent.vue
+++ viernes-vue/src/components/MovieDetails/MovieCastComponent.vue
@@ -6,17 +6,25 @@
           <h2 class="main__titulo">Elenco</h2>
         </div>
       </div>
-      <div class="row">
-        <figure class="pelicula__actor col-6 col-sm-4">
-          <img src="" alt="" />
-          <figcaption class="pelicula__actor__name text-center"></figcaption>
+      <div class="row" v-if="currentMovie">
+        <figure v-for="actor in currentMovie.cast" :key="actor" class="pelicula__actor col-6 col-sm-4">
+          <img :src="currentMovie.cast.img" :alt="currentMovie.cast.alt" />
+          <figcaption class="pelicula__actor__name text-center">{{ currentMovie.cast.alt }}</figcaption>
         </figure>
       </div>
     </div>
 
     <ul v-if="currentMovie">
-      <li v-for="actor in currentMovie.cast" :key="actor">{{ actor }}</li>
+      <li v-for="image in currentMovie.cast_2" :key="image.alt">{{ image }}</li>
     </ul>
+
+    <div v-if="currentMovie">
+      <p>{{ currentMovie.cast_2[0] }}</p>
+    </div>
+
+    <!-- <ul v-if="currentMovie">
+      <li v-for="actor in currentMovie.cast" :key="actor">{{ actor.img }} - {{ actor.actor }}</li>
+    </ul> -->
   </section>
 </template>
 
@@ -28,9 +36,6 @@ export default {
       type: Object,
       required: true,
     },
-  },
-  mounted() {
-    console.log(this.currentMovie);
   },
 };
 </script>
--- viernes-vue/src/views/MovieDetailsView.vue
+++ viernes-vue/src/views/MovieDetailsView.vue
@@ -16,6 +16,7 @@
 
     <!-- Cast -->
     <MovieCastComponent :currentMovie="currentMovie"></MovieCastComponent>
+
   </main>
 </template>
 
