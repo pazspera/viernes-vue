27998f94e1ad2f3cc0f65694d7ae6cff62beede2
Paz Spera <mspera03@gmail.com>
2022-12-02 02:31:20 -0300

    Agregué cards con los años a HistorialView

--- viernes-vue/src/views/HistorialView.vue
+++ viernes-vue/src/views/HistorialView.vue
@@ -7,6 +7,24 @@
       <p class="hero-grid__texto-final">Todos los viernes todos.</p>
     </div>
   </section>
+
+  <!-- Main -->
+  <main class="main-content">
+    <div class="main-text">
+      <div class="container">
+        <div class="row">
+          <div v-for="year in years" :key="year" class="card card-pelicula card-vertical col-12 col-sm-6 col-lg-4">
+            <div class="card-body card-vertical__contenido">
+              <h2 class="card-vertical__titulo">
+                <a href="#" class="stretched-link">{{ year.year }}</a>
+              </h2>
+            </div>
+            <img :="year.imgYear">
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -18,6 +36,29 @@ export default {
   data() {
     return {
       heroBackgroundImg: "https://github.com/pazspera/viernes-img/blob/main/hero/hero_historial_peliculas.jpg?raw=true",
+      years: [
+        {
+          year: "2020",
+          imgYear: {
+            src: "https://github.com/pazspera/viernes-img/blob/main/years/2020-brazil.jpg?raw=true",
+            alt: "Poster Brazil",
+          },
+        },
+        {
+          year: "2021",
+          imgYear: {
+            src: "https://github.com/pazspera/viernes-img/blob/main/years/2021-mulholland.jpg?raw=true",
+            alt: "Poster Mulholland Drive",
+          },
+        },
+        {
+          year: "2022",
+          imgYear: {
+            src: "https://github.com/pazspera/viernes-img/blob/main/years/2022-from-dusk.jpg?raw=true",
+            alt: "Poster From Dusk Till Dawn",
+          },
+        },
+      ],
     };
   },
 };
