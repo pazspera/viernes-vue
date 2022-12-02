44b429c0dbddd0ff57162a97484e7c24e9a484b9
Paz Spera <mspera03@gmail.com>
2022-12-02 02:18:24 -0300

    Agregué hero historialView

--- viernes-vue/src/views/HistorialView.vue
+++ viernes-vue/src/views/HistorialView.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1 class="text-danger">Historial view</h1>
+  <!-- Hero -->
+  <section class="hero-grid">
+    <div class="hero-grid__img" :style="{ backgroundImage: `url(${heroBackgroundImg})` }"></div>
+    <div class="hero-grid__cartel hero-grid__cartel--cartel-corto">
+      <h1 class="hero-grid__titulo">Historial de películas</h1>
+      <p class="hero-grid__texto-final">Todos los viernes todos.</p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -7,6 +14,11 @@ export default {
   name: "HistorialView",
   mounted() {
     document.title = "Historial de películas - Viernes";
+  },
+  data() {
+    return {
+      heroBackgroundImg: "https://github.com/pazspera/viernes-img/blob/main/hero/hero_historial_peliculas.jpg?raw=true",
+    };
   },
 };
 </script>
